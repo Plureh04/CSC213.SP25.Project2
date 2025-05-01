e52b5ee2c2e154ded7ca69f204438d7068e7a6b8
PayPlu <plureh07@gmail.com>
2025-05-01 15:29:19 -0400

    Update ComplaintMerger.java

--- src/main/java/edu/canisius/csc213/complaints/storage/ComplaintMerger.java
+++ src/main/java/edu/canisius/csc213/complaints/storage/ComplaintMerger.java
@@ -14,7 +14,14 @@ public class ComplaintMerger {
      * @param embeddings Map from complaintId to embedding vector (from JSONL)
      */
     public static void mergeEmbeddings(List<Complaint> complaints, Map<Long, double[]> embeddings) {
-        // TODO: For each complaint, match the ID to an embedding and set it
+        for (Complaint complaint : complaints) {
+            double[] embedding = embeddings.get(complaint.getComplaintId());
+            if (embedding != null) {
+                complaint.setEmbedding(embedding);
+            }
+        }
     }
+
+
 
 }
